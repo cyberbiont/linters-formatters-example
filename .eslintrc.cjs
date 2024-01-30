9546f358d934c20ff9a3dc6b809ac36b33fd4e30
cyberbiont <cyberbiont@gmail.com>
2024-01-30 16:53:09 +0200

    fix: Fix linter configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,35 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["standard-with-typescript", "prettier"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    // add your on rules here that will override extended config
-    "prefer-const": [
-      "off",
-      {
-        destructuring: "any",
-        ignoreReadBeforeAssign: false,
-      },
-    ],
-    // "prefer-const": ["error", {
-    //     "destructuring": "any",
-    //     "ignoreReadBeforeAssign": false
-    // }]
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['standard-with-typescript', 'prettier'],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		// add your on rules here that will override extended config
+		// 'prefer-const': [
+		// 	'off'
+		// ],
+		'prefer-const': [
+			'error',
+			{
+				destructuring: 'any',
+				ignoreReadBeforeAssign: false,
+			},
+		],
+	},
 };
