1ac4c71b3b99b0005f14b19620264303da012d92
cyberbiont <cyberbiont@gmail.com>
2024-01-30 19:06:10 +0200

    refactor: Test commit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const message: string = 'Hello Typescript!';
 console.log(message);
 
 // const arr = [1, 2, 3, 4];
-let arr = [1, 2, 3, 4];
+const arr = [1, 2, 3, 4];
 
 for (const val of arr) {
 	console.log(val);
